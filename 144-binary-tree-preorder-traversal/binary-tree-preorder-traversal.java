cf00ffc69138b2a49d0a637e92bbdfca2cd7f0e5
Sharan I <sharan052006@gmail.com>
2026-09-20 18:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (14.97%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(res, root);
+        return res;
+    }
+    public void traversal(List<Integer> res, TreeNode temp){
+        if(temp==null){
+            return;
+        }
+        res.add(temp.val);
+        traversal(res,temp.left);
+        traversal(res,temp.right);
+    }
+}
